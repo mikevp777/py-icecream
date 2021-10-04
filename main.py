bea20557a62b8ce2825a962f2d3e06e47e7b832a
mikevp777 <mikevp777@gmail.com>
2021-10-04 23:07:56 +0300

    Вынес проверку в функцию, добавил исключение

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
+class CantSolve(Exception):
+    """Can't solve ax+by=k in integers"""
+
+
+def solve_integer(a, b, k):
+    if k % b == 0:
+        return 0, k // b
+    x_a = k - k % a
+    y_b = k - x_a
+    while y_b % b != 0:
+        x_a -= a
+        y_b += a
+        if x_a < a:
+            raise CantSolve
+    return x_a//a, y_b//b
+
+
 def main():
     print('Решение уравнения ax+by=k в целых x и у')
     k = int(input('k: '))
     a = int(input('a: '))
     b = int(input('b: '))
-    if k % b != 0:
-        x_a = k - k % a
-        x_b = k - x_a
-        while x_b % b != 0:
-            x_a -= a
-            x_b += a
-            if x_a < a:
-                print('Разложить нельзя')
-                return
+    try:
+        x, y = solve_integer(a, b, k)
+    except CantSolve:
+        print('Не раскладывается')
     else:
-        x_a = 0
-        x_b = k
-
-    print(f'Раскладывается как {x_a//a}*{a} + {x_b//b}*{b} = {k}')
+        print(f'Раскладывается как {x}*{a} + {y}*{b} = {k}')
 
 
 if __name__ == '__main__':
