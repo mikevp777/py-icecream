ae10d36161a17a8fe1da506bb9cfc08de8608a47
mikevp777 <mikevp777@gmail.com>
2021-10-04 23:36:53 +0300

    Добавил комментарии

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ class CantSolve(Exception):
 
 
 def solve_integer(a, b, k):
+
+    # Проверка условия: "k делится на b нацело"
+    # без этой проверки в некоторых случаях алгоритм не сработает
+    # Если делится - решение найдено
+
     if k % b == 0:
         return 0, k // b
+
+    # Сам алгоритм (проверка что k делится на a нацело в нем уже заложена):
+
     x_a = k - k % a
     y_b = k - x_a
     while y_b % b != 0:
         x_a -= a
         y_b += a
         if x_a < a:
-            raise CantSolve
+            raise CantSolve  # если решения нет - вызывает исключение
     return x_a//a, y_b//b
 
 
