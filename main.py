e4c214ac41d27d5bd12e5be18a9eac243c7b18ab
mikevp777 <mikevp777@gmail.com>
2021-10-04 22:27:04 +0300

    Учтен один нюанс

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ def main():
     k = int(input('k: '))
     a = int(input('a: '))
     b = int(input('b: '))
-    x_a = k - k % a
-    x_b = k - x_a
-    while x_b % b != 0:
-        x_a -= a
-        x_b += a
-        if x_a < a:
-            print('Разложить нельзя')
-            return
+    if k % b != 0:
+        x_a = k - k % a
+        x_b = k - x_a
+        while x_b % b != 0:
+            x_a -= a
+            x_b += a
+            if x_a < a:
+                print('Разложить нельзя')
+                return
+    else:
+        x_a = 0
+        x_b = k
 
     print(f'Раскладывается как {x_a//a}*{a} + {x_b//b}*{b} = {k}')
 
